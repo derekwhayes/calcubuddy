a2b6fe36c4e073ccfc9d8e35f25a9f0c8abf9d73
Derek Hayes <derek2099@gmail.com>
2024-09-27 06:27:17 -0500

    Add operate function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+let input1;
+let input2;
+let plus;
+let minus;
+let mult;
+let div;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,4 +21,18 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-console.log(divide(2, 4));
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case plus:
+            return add(num1, num2);
+        case minus:
+            return subtract(num1, num2);
+        case mult:
+            return multiply(num1, num2);
+        case div:
+            return divide(num1, num2);
+        default:
+            return 'ERROR';
+    }
+}
+
